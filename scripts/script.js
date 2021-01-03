461eff746c329f277ff98a60511817c3b4c36f0e
r3m00n <merlin.foerster@live.de>
2021-01-04 00:04:00 +0100

    🛫 sideScroll & fix Img done

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,44 @@
-const pageButtons = document.querySelector("#page-buttons")
+const logoFix = document.querySelector("#logo-fix")
+const aside = document.querySelector("#page-buttons")
+
+const allBoxes = document.querySelectorAll(".box")
+const boxOne = document.querySelector("#button-one")
+const boxTwo = document.querySelector("#button-two")
+const boxThree = document.querySelector("#button-three")
+const boxFour = document.querySelector("#button-four")
+const boxFive = document.querySelector("#button-five")
+
 const allLines = document.querySelectorAll(".line")
 const lineTwo = document.querySelector("#line-two")
 const lineThree = document.querySelector("#line-three")
 const lineFour = document.querySelector("#line-four")
 const lineFive = document.querySelector("#line-five")
 
-window.onscroll = () => scrollFunction()
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    
+    if (this.scrollY > 199) {
+        activate(aside)
+        activate(logoFix)
+    } else {
+        deactivate(aside)
+        deactivate(logoFix)
+    }
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    // document.getElementById("header").style.fontSize = "30px";
-  } else {
-    // document.getElementById("header").style.fontSize = "90px";
-  }
-}
+    removeActiveBoxes()
+    removeActiveLines()
+    
+    if (this.scrollY < 866 ) activate(lineTwo)
+    if (this.scrollY > 866 && this.scrollY < 1060 ) activate(boxTwo)
+    if (this.scrollY > 1060 && this.scrollY < 1750 ) activate(lineThree)
+    if (this.scrollY > 1750 && this.scrollY < 1960 ) activate(boxThree)
+    if (this.scrollY > 1960 && this.scrollY < 2570 ) activate(lineFour)
+    if (this.scrollY > 2570 && this.scrollY < 3060 ) activate(boxFour)
+    if (this.scrollY > 3060 && this.scrollY < 3560 ) activate(lineFive)
+    if (this.scrollY > 3560) activate(boxFive)
+})
+
+removeActiveBoxes = () => allBoxes.forEach((box) => deactivate(box))
+removeActiveLines = () => allLines.forEach((line) => deactivate(line))
+activate = (e) => e.classList.add("active") 
+deactivate = (e) => e.classList.remove("active") 
