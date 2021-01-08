c23b6a99e1d3de3cd5cdbb2f85877ed726251f0b
r3m00n <merlin.foerster@live.de>
2021-01-08 18:33:27 +0100

    😭 bevor ich alles kaputt mache

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// const wrapper = document.querySelector("#scroll-wrapper")
 const logoFix = document.querySelector("#logo-fix")
 const aside = document.querySelector("#page-buttons")
 
@@ -15,8 +16,6 @@ const lineFour = document.querySelector("#line-four")
 const lineFive = document.querySelector("#line-five")
 
 window.addEventListener("scroll", () => {
-    console.log(window.scrollY);
-    
     if (this.scrollY > 199) {
         activate(aside)
         activate(logoFix)
@@ -28,10 +27,11 @@ window.addEventListener("scroll", () => {
     removeActiveBoxes()
     removeActiveLines()
     
+    // boxOne machen
     if (this.scrollY < 866 ) activate(lineTwo)
     if (this.scrollY > 866 && this.scrollY < 1060 ) activate(boxTwo)
-    if (this.scrollY > 1060 && this.scrollY < 1750 ) activate(lineThree)
-    if (this.scrollY > 1750 && this.scrollY < 1960 ) activate(boxThree)
+    if (this.scrollY > 1060 && this.scrollY < 1700 ) activate(lineThree)
+    if (this.scrollY > 1700 && this.scrollY < 1960 ) activate(boxThree)
     if (this.scrollY > 1960 && this.scrollY < 2570 ) activate(lineFour)
     if (this.scrollY > 2570 && this.scrollY < 3060 ) activate(boxFour)
     if (this.scrollY > 3060 && this.scrollY < 3560 ) activate(lineFive)
